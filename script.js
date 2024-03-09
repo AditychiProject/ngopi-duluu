e53c0e6e2e54444ca961d9664271dd2dd883c0d8
AditychiProject <aditychiproject@gmail.com>
2024-03-09 07:44:39 +0700

    Melakukan perubahan pada kode

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-// Menu Active
+// Hamburger Menu
 const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Close Sidebar
+// Close Outside of Element
 const hamburger = document.querySelector("#hamburger-menu");
+const searchButton = document.querySelector("#search-button");
 document.addEventListener("click", function (e) {
+  // Hamburger Menu
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+
+  // Search Bar
+  if (!searchButton.contains(e.target) && !searchBar.contains(e.target)) {
+    searchBar.classList.remove("active");
+  }
 });
+
+// Search Bar
+const searchBar = document.querySelector(".search-bar");
+const searchBox = document.querySelector("#search-box");
+document.querySelector("#search-button").onclick = () => {
+  searchBar.classList.toggle("active");
+  searchBox.focus();
+};
